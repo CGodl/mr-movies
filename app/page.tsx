302b6a6efc618f08611d2c6c9ee3833328209331
Carl Godlewski <cgodlew@gmail.com>
2026-09-22 18:48:45 -0400

    Added search and filter logic. Added biggest font for Mr. Movie title.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { MovieCard } from "@/components/MovieCard";
+import { MovieCard } from "@/components/Card/MovieCard";
+import { MovieGrid } from "@/components/MovieGrid";
 
 import { fetchMovies } from "@/data/fetchMovies";
 import { Movie } from "@/types/movies";
@@ -7,16 +8,9 @@ export default async function HomePage() {
   const movies = await fetchMovies();
   return (
     <main>
-      <h1>Mr. Movies 2025</h1>
+      <h1 className="text-5xl">Mr. Movies 2025</h1>
       <p>The best website to find movies focused on 2025 and 2025 only</p>
-      <ul className="grid grid-cols-2 gap-4 p-2 sm:grid-cols-3 lg:grid-cols-6">
-        {movies.map((movie: Movie) => (
-          <li key={movie.id}>
-            <MovieCard movie={movie} />
-          </li>
-        ))}
-      </ul>
-
+      <MovieGrid movies={movies} />
     </main>
   );
 }
